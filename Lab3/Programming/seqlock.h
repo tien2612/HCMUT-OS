956482f38be709f8dc3952034099e7f250d56179
tien2612 <nguyenphuctien1258@gmail.com>
2022-10-31 11:49:57 +0700

    done ex3, half ex2

---
+++ Lab3/Programming/seqlock.h
@@ -0,0 +1,14 @@
+/*
+ * seqlock.h
+ *
+ *  Created on: Oct 30, 2022
+ *      Author: Nguyen Phuc Tien - 2014725
+ */
+
+#ifndef INC_SEQLOCK_H_
+#define INC_SEQLOCK_H_
+
+typedef struct {}
+;
+
+#endif /* INC_SEQLOCK_H_ */
